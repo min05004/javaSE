a2ef1fd286b5217b912cc5a576d5a2d7ea70cd93
hmy50 <hmy50@192.168.219.104>
2023-07-23 21:28:07 +0900

    숙제

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -9,5 +9,84 @@ package section05;
  * 
  */
 public class LoopHomeWork {
+	public static void main(String[] args) {
+		/*
+		 * 2번
 
-}
+   *
+  **
+ ***
+****
+
+3번
+   *
+  ***
+ *****
+******* 
+  
+4번
+
+   *				
+  ***				  
+ *****				
+*******		
+ *****		 
+  ***		 
+   *		
+   
+		 */
+		//2번
+	/* for(int i =1 ; i < 5 ; i++) {
+			for(int j = 4; j >0 ; j--) {
+			if(i< j) {
+				System.out.print(" ");
+			} else {
+				System.out.print("*");
+			}
+			
+		}
+			System.out.println();
+			*/
+		//3번
+	/* for(int i =0 ; i < 5 ; i++) {
+		for(int j =5; j > i+1; j--) {
+	
+			System.out.print(" ");
+			}
+		for(int s =0; s < i*2+1 ; s++) {
+			System.out.print("*");
+		
+		}
+		System.out.println( );
+		}
+		*/
+		//4번
+		 for(int i = 0; i < 5; i++) {
+			for(int j =5; j > i+1; j--) {
+				System.out.print(" ");
+			}
+			for(int s =0; s < i*2+1 ; s++) {
+				System.out.print("*");
+			}
+			System.out.println( );
+		}
+		
+		for(int k = 1; k < 5; k++) {
+			for( int d =1; d < k+1; d++) {
+				System.out.print(" ");
+			}
+			for(int f =10; f > k*2+1; f--) {
+				System.out.print("*");
+				
+			}
+			System.out.println( );
+		}
+		
+	
+	}
+	}
+
+				
+
+
+
