3be9c6a60eba9df6de76ffc1acd0a6d489b864bf
TG <TG@THM>
2023-07-28 18:07:10 +0900

    연습

--- src/section06/Array04.java
+++ src/section06/Array04.java
@@ -8,12 +8,15 @@ public class Array04 {
 		nums[0][0] = 1;
 		nums[0][1] = 2;
 		nums[0][2] = 3;
+		
 		nums[1][0] = 4;
 		nums[1][1] = 5;
 		nums[1][2] = 6;
+		
 		nums[2][0] = 7;
 		nums[2][1] = 8;
 		nums[2][2] = 9;
+		
 		nums[3][0] = 10;
 		nums[3][1] = 11;
 		nums[3][2] = 12;
--- src/section06/ArrayHomWork.java
+++ src/section06/ArrayHomWork.java
@@ -22,7 +22,7 @@ package section06;
  2. for문으로 1~49까지 대입하기
  3. 아래 모양으로 출력하기!!!!!!!!!!!!!!
 
-1
+1 
 2 8
 15 9 3
 4 10 16 22
@@ -38,34 +38,56 @@ package section06;
  
  * 
  */
+//지그재그로 나온다는게 무슨말일까..?
+/*
+ *  for(int s = 0; s<4 ; s++) {; //1번 학생, 2번학생 ...반복 4번되는 값을 s로 둠.
+					System.out.println((s+1) + "번 학생 점수---");
+				for(int i=0; i<3 ; i++){
+					System.out.println(subject[i]+":"); //subject 값이 0,1,2,scores값이 0,1,2 반복되기 때문에 반복문을 사용.
+					scores[s][i]=sc.nextInt();
+					
+ */
 public class ArrayHomWork {
 	public static void main(String[] args) {
 		
-		int nums[][] = {
-					{1 , 2 , 3 , 4 , 5 , 6 , 7},
-					{8 , 9 , 10 ,11, 12 ,13, 14},
-					{15, 16, 17, 18, 19, 20, 21},
-					{22, 23, 24, 25, 26, 27, 28},
-					{29, 30, 31, 32, 33, 34, 35},
-					{36, 37, 38, 39, 40, 41, 42},
-					{43, 44, 45, 46, 47, 48, 49},
-			
-					};
+		int nums[][] = new int[7][7];
+		int arr=1;
 		
-		for(int i =0; i <nums.length; i++) {
-			if(i%2!=0) { 
-//				for(int )
-				System.out.println();
-			} else {
+		
+		for(int i = 0; i < 7; i++) {
+			System.out.print(i);
 			
-			for(int j=0; j <nums[i].length; j++) {
-				System.out.print(nums[i][j]+" "); 
+			for(int j=0; j<7; j++) {
+				System.out.print(j+1);
+		
+		}
 	}
-			System.out.println();
+	}}
+				
+		
+				
+		
+		/*int [0][0] 1
+		 * int [0][1] 2 int [1][0] 8
+		 * int [0][3] 15  int [1][1] 9  [3][0] 3 
+		 * [4][0] [
+		 */
+	
+	/*	for(int i = 0; i < nums.length; i++) {
+			for(int j = 0; j <nums[i].length; j++) {
+				i-1
+				
+				System.out.print (nums[i][j]+" "); // nums[i][j] 를 표현
+			}
+		System.out.println();
+		
 	}
 	}
-	}
+
 }
+		*/
+		
+		
 	
 			
 
