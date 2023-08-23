601d39649464a25201b5b22bc00c8325201951f5
ITSC <ITSC@DESKTOP-ONE4R81>
2023-08-23 19:09:12 +0900

    test

--- src/section19/Escape.java
+++ src/section19/Escape.java
@@ -15,6 +15,8 @@ public class Escape {
 	public static void main(String[] args) {
 		System.out.println("\"hello\" \f");
 		System.out.println("\"hello\" \f");
+		System.out.println("\"hello\" \f");
+		System.out.println("\"hello\" \f");
 	}
 
 }
